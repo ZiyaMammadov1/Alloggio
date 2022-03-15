d60107c45d74b69dd96b596d465d677be3891b1e
ZiyaMammadov1 <ziyaam@code.edu.az>
2022-03-15 19:39:19 +0400

    Create admin and login is ok

---
+++ Alloggio MVC/Areas/Manage/Controllers/AccountController.cs
@@ -0,0 +1,71 @@
+﻿using Alloggio_MVC.Areas.Manage.ViewModels;
+using Core_Layer.Entities;
+using Data_Layer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Alloggio_MVC.Areas.Manage.Controllers
+{
+    [Area("manage")]
+    public class AccountController : Controller
+    {
+        private readonly DataContext _context;
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signManager;
+
+        public AccountController(DataContext context, UserManager<AppUser> userManager, SignInManager<AppUser> signManager)
+        {
+            _context = context;
+            _userManager = userManager;
+            _signManager = signManager;
+        }
+        public async Task<IActionResult> Index()
+        {
+            AppUser Superadmin = new AppUser
+            {
+                UserName = "Superadmin",
+                Fullname = "Super Admin",
+                Email = "Superadmin@outlook.com"
+            };
+            var createresult = await _userManager.CreateAsync(Superadmin, "Admin123");
+
+            return Ok(createresult.Errors);
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(AdminLoginViewModel AdminLoginVM)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(AdminLoginVM);
+            }
+
+            AppUser Admin = await _userManager.FindByNameAsync(AdminLoginVM.Username);
+
+            if (Admin == null)
+            {
+                ModelState.AddModelError("", "Username or Password incorrect");
+                return View(AdminLoginVM);
+            }
+
+            if ( !await _userManager.CheckPasswordAsync(Admin, AdminLoginVM.Password))
+            {
+                ModelState.AddModelError("", "Username or Password incorrect");
+                return View(AdminLoginVM);
+            }
+
+            var singIn = await _signManager.PasswordSignInAsync(Admin, AdminLoginVM.Password, false, false);
+
+            return RedirectToAction("index","dashboard");
+        }
+    }
+}
---
+++ Alloggio MVC/Areas/Manage/ViewModels/AdminLoginViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Alloggio_MVC.Areas.Manage.ViewModels
+{
+    public class AdminLoginViewModel
+    {
+        [Required]
+        [StringLength(maximumLength: 20)]
+        public string Username { get; set; }
+
+        [Required]
+        [StringLength(maximumLength: 25)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+
+    }
+}
