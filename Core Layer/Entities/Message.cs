96d860b3be896c8805ed3b94a13a32c8b1a5bed5
ZiyaMammadov1 <ziyaam@code.edu.az>
2022-03-27 14:12:44 +0400

    Message limits added

--- Core Layer/Entities/Message.cs
+++ Core Layer/Entities/Message.cs
@@ -8,8 +8,14 @@ namespace Core_Layer.Entities
     public class Message
     {
         public int id { get; set; }
+
+        [StringLength(maximumLength:50)]
         public string FullName { get; set; }
+
+        [StringLength(maximumLength: 50)]
         public string Email { get; set; }
+
+        [StringLength(maximumLength: 300)]
         public string Content { get; set; }
     }
 }
---
+++ Data Layer/Migrations/20220327101114_UpdateMessageTable.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Data_Layer.Migrations
+{
+    public partial class UpdateMessageTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "FullName",
+                table: "Messages",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Messages",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Content",
+                table: "Messages",
+                maxLength: 300,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "FullName",
+                table: "Messages",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 50,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Messages",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 50,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Content",
+                table: "Messages",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 300,
+                oldNullable: true);
+        }
+    }
+}
