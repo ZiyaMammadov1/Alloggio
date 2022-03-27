23d41d7c5c586a6bff6200cffe3cfdb0de16382b
ZiyaMammadov1 <ziyaam@code.edu.az>
2022-03-27 14:00:12 +0400

    Settings limits added

--- Core Layer/Entities/Setting.cs
+++ Core Layer/Entities/Setting.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Core_Layer.Entities
@@ -7,7 +8,11 @@ namespace Core_Layer.Entities
     public class Setting
     {
         public int id { get; set; }
+
+        [StringLength(maximumLength:150)]
         public string Key { get; set; }
+
+        [StringLength(maximumLength: 500)]
         public string Value { get; set; }
     }
 }
---
+++ Data Layer/Migrations/20220327095814_UpdateSettingTable.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Data_Layer.Migrations
+{
+    public partial class UpdateSettingTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Value",
+                table: "Settings",
+                maxLength: 500,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Key",
+                table: "Settings",
+                maxLength: 150,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Value",
+                table: "Settings",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 500,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Key",
+                table: "Settings",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 150,
+                oldNullable: true);
+        }
+    }
+}
