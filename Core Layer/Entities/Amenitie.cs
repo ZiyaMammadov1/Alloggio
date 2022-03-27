6fd7b65cc9f4909bc60ab6f16fdc39761d5abb32
ZiyaMammadov1 <ziyaam@code.edu.az>
2022-03-27 13:42:51 +0400

    Amenities limits added

--- Core Layer/Entities/Amenitie.cs
+++ Core Layer/Entities/Amenitie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Core_Layer.Entities
@@ -7,7 +8,11 @@ namespace Core_Layer.Entities
     public class Amenitie
     {
         public int id { get; set; }
+
+        [StringLength(maximumLength:100)]
         public string Image { get; set; }
+
+        [StringLength(maximumLength: 50)]
         public string Name { get; set; }
         public List<RoomAmenities> RoomAmenities { get; set; }
 
---
+++ Data Layer/Migrations/20220327094110_UpdateAmenitiesTable.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Data_Layer.Migrations
+{
+    public partial class UpdateAmenitiesTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Amenities",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Image",
+                table: "Amenities",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Amenities",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 50,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Image",
+                table: "Amenities",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 100,
+                oldNullable: true);
+        }
+    }
+}
